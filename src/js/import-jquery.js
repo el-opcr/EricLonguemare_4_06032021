e8467dff260a60b81a3c627346f605437249195d
el-opcr <el.openclassrooms@gmail.com>
2021-03-20 23:32:04 +0100

    use webp pictures

--- src/js/import-jquery.js
+++ src/js/import-jquery.js
@@ -2,4 +2,9 @@
 import * as jquery from 'jquery';
 export default (window.$ = window.jQuery = jquery);
 import image1 from "url:../img/1.jpg?as=webp";
+import image2 from "url:../img/2.jpg?as=webp";
+import image3 from "url:../img/3.jpg?as=webp";
+import image4 from "url:../img/4.jpg?as=webp";
+
+
 
