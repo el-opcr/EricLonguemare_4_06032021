9134f5b29a6b49206ad5f4059c00c6b50d8edf79
el-opcr <el.openclassrooms@gmail.com>
2021-03-21 04:17:11 +0100

    build

--- src/js/import-jquery.js
+++ src/js/import-jquery.js
@@ -5,6 +5,3 @@ import image1 from "url:../img/1.jpg?as=webp";
 import image2 from "url:../img/2.jpg?as=webp";
 import image3 from "url:../img/3.jpg?as=webp";
 import image4 from "url:../img/4.jpg?as=webp";
-
-
-
